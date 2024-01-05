52a963f4a0f502f5a5788d9a9340347b0cdd29e4
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-05 12:01:25 +0530

    Conditional rendering(ternary operator)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,18 +7,25 @@ function App() {
     channel: "Code With Harry",
     title: "React JS tutorial",
     time: "10 months ago",
+    verified: true,
   };
   return (
     <div className="App">
       <div className="title">Video</div>
       <Video {...obj}></Video>
       <Video
+        verified={false}
         views="400K"
         channel="Chai aur Code"
         title="Node JS tutorial"
         time="1 year ago"
       ></Video>
-      <Video views="100K" title="Next JS tutorial" time="2 year ago"></Video>
+      <Video
+        verified={true}
+        views="100K"
+        title="Next JS tutorial"
+        time="2 year ago"
+      ></Video>
     </div>
   );
 }
--- my-app/src/components/Video.js
+++ my-app/src/components/Video.js
@@ -1,18 +1,22 @@
 import "./Video.css";
 
-function Video({ title, channel = "Code With Harry", views, time }) {
+function Video({ title, channel = "Code With Harry", views, time, verified }) {
   //channel = "Code With Harry" to make default value...if channel name is missing in App.js
+
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img src="https://dummyimage.com/600x300/000/fff" alt="React.js" />
+          <img src="https://picsum.photos/id/2/160/90" alt="React.js" />
         </div>
 
         <div className="title">{title}</div>
-        <div className="channel">{channel}</div>
+
+        <div className="channel">
+          {channel} {verified ? "✅" : null}{" "}
+        </div>
         <div className="views">
-          {views} views <span>.</span> {time}
+          {views} views <span>•</span> {time}
         </div>
       </div>
     </>
