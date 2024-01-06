b26a38205f982998300237dae60228205f030708
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-06 11:30:16 +0530

    List rendering...using props

--- my-app/src/components/Video.js
+++ my-app/src/components/Video.js
@@ -1,19 +1,26 @@
 import "./Video.css";
 
-function Video({ title, channel = "Code With Harry", views, time, verified }) {
+function Video({
+  title,
+  id,
+  channel = "Code With Harry",
+  views,
+  time,
+  verified,
+}) {
   //channel = "Code With Harry" to make default value...if channel name is missing in App.js
 
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img src="https://picsum.photos/id/2/160/90" alt="React.js" />
+          <img src={`https://picsum.photos/id/${id}/250/150`} alt="React.js" />
         </div>
 
         <div className="title">{title}</div>
 
         <div className="channel">
-          {channel} {verified ? "✅" : null}{" "}
+          {channel} {verified && "✅"}{" "}
         </div>
         <div className="views">
           {views} views <span>•</span> {time}
