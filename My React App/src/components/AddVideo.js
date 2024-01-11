c610d45bc1cfc2f1561b6310a81acf95363208d1
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-11 09:46:53 +0530

    Updated and corrected

--- My React App/src/components/AddVideo.js
+++ My React App/src/components/AddVideo.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import "./AddVideo.css";
 
-const AddVideo = (addVideos) => {
+const AddVideo = ({ addVideos }) => {
   const [video, setVideo] = useState({
     channel: "Code With Harry",
     title: "React JS tutorial",
@@ -11,12 +11,9 @@ const AddVideo = (addVideos) => {
   function handleSubmit(e) {
     e.preventDefault();
     addVideos(video);
-    console.log(video);
   }
   function handleChange(e) {
-    console.log(e.target.name, e.target.value);
     setVideo({ ...video, [e.target.name]: e.target.value });
-    console.log(video);
   }
   return (
     <form>
