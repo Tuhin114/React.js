d533b70b170b99f9405220887a4d4041dabaca91
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-11 09:53:59 +0530

    Auto clean form after submit

--- My React App/src/components/AddVideo.js
+++ My React App/src/components/AddVideo.js
@@ -1,16 +1,20 @@
 import { React, useState } from "react";
 import "./AddVideo.css";
 
+const initialVideos = {
+  channel: "Code With Harry",
+  time: "10 months ago",
+  verified: true,
+  title: "",
+  views: "",
+};
+
 const AddVideo = ({ addVideos }) => {
-  const [video, setVideo] = useState({
-    channel: "Code With Harry",
-    title: "React JS tutorial",
-    time: "10 months ago",
-    verified: true,
-  });
+  const [video, setVideo] = useState({ initialVideos });
   function handleSubmit(e) {
     e.preventDefault();
     addVideos(video);
+    setVideo(initialVideos);
   }
   function handleChange(e) {
     setVideo({ ...video, [e.target.name]: e.target.value });
@@ -22,12 +26,14 @@ const AddVideo = ({ addVideos }) => {
         name="title"
         onChange={handleChange}
         placeholder="title"
+        value={video.title}
       />
       <input
         type="text"
         name="views"
         onChange={handleChange}
         placeholder="views"
+        value={video.views}
       />
       <button onClick={handleSubmit}>Add Video</button>
     </form>
